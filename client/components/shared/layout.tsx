f66c5b1f788e4af670b0e8d07f3653280b25e42d
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2023-02-19 15:37:13 +0530

    feat(client): alert banner

--- client/components/shared/layout.tsx
+++ client/components/shared/layout.tsx
@@ -4,6 +4,11 @@ import { Topbar } from "./";
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col mx-auto min-h-screen h-full">
+      <aside className="h-10 text-white font-bold bg-red-600 text-center px-28 flex items-center justify-center">
+        ⚠️ Please note that this platform is not compatible with PolygonID
+        mobile application version 2.0.
+      </aside>
+
       <Topbar />
 
       <main className="flex w-full h-full">{children}</main>
